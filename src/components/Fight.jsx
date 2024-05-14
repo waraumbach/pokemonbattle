e64aa251a3e47fc0b85f920893bd3b2b41791d5e
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-05-14 14:18:19 +0200

    added useLocation

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -1,63 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
 function Fight() {
-    const [pokemonList, setPokemonList] = useState([]);
-    const [selectedPokemon, setSelectedPokemon] = useState(null);
-    const [enemyPokemon, setEnemyPokemon] = useState(null);
-    const [battleResult, setBattleResult] = useState('');
+  let { state } = useLocation();
+  console.log(state);
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [enemyPokemon, setEnemyPokemon] = useState(null);
+  const [battleResult, setBattleResult] = useState("");
 
-    // Fetching list of Pokemon from API
-    useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon')
-            .then(response => {
-                setPokemonList(response.data.results);
-            })
-            .catch(error => {
-                console.error('Error fetching Pokemon:', error);
-            });
-    }, []);
+  // Fetching list of Pokemon from API
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/${state.id}`)
+      .then((response) => {
+        setPokemonList(response.data.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching Pokemon:", error);
+      });
+  }, []);
 
-    // Function to select a random Pokemon from the list
-    const selectRandomPokemon = () => {
-        const randomIndex = Math.floor(Math.random() * pokemonList.length);
-        setSelectedPokemon(pokemonList[randomIndex]);
-    };
+  // Function to select a random Pokemon from the list
+  const selectRandomPokemon = () => {
+    const randomIndex = Math.floor(Math.random() * pokemonList.length);
+    setSelectedPokemon(pokemonList[randomIndex]);
+  };
 
-    // Function to start a battle with a randomly selected enemy Pokemon
-    const startBattle = () => {
-        selectRandomPokemon();
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random() * 15)}`)
-            .then(response => {
-                setEnemyPokemon(response.data);
-                // Simulate battle result (for demonstration purposes)
-                const result = Math.random() < 0.5 ? 'You win!' : 'You lose!';
-                setBattleResult(result);
-            })
-            .catch(error => {
-                console.error('Error fetching enemy Pokemon:', error);
-            });
-    };
+  // Function to start a battle with a randomly selected enemy Pokemon
+  const startBattle = () => {
+    selectRandomPokemon();
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random() * 15)}`)
+      .then((response) => {
+        setEnemyPokemon(response.data);
+        // Simulate battle result (for demonstration purposes)
+        const result = Math.random() < 0.5 ? "You win!" : "You lose!";
+        setBattleResult(result);
+      })
+      .catch((error) => {
+        console.error("Error fetching enemy Pokemon:", error);
+      });
+  };
 
-    return (
+  return (
+    <div>
+      <h1>Pokemon Game</h1>
+
+      <button onClick={startBattle}>Start Battle</button>
+      {selectedPokemon && (
         <div>
-            <h1>Pokemon Game</h1>
-            <button onClick={startBattle}>Start Battle</button>
-            {selectedPokemon && (
-                <div>
-                    <h2>Your Pokemon: {selectedPokemon.name}</h2>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.url.split('/')[6]}.png`} alt={selectedPokemon.name} />
-                </div>
-            )}
-            {enemyPokemon && (
-                <div>
-                    <h2>Enemy Pokemon: {enemyPokemon.name}</h2>
-                    <img src={enemyPokemon.sprites.front_default} alt={enemyPokemon.name} />
-                </div>
-            )}
-            {battleResult && <h3>{battleResult}</h3>}
+          <h2>Your Pokemon: {selectedPokemon.name}</h2>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              selectedPokemon.url.split("/")[6]
+            }.png`}
+            alt={selectedPokemon.name}
+          />
         </div>
-    );
+      )}
+      {enemyPokemon && (
+        <div>
+          <h2>Enemy Pokemon: {enemyPokemon.name}</h2>
+          <img
+            src={enemyPokemon.sprites.front_default}
+            alt={enemyPokemon.name}
+          />
+        </div>
+      )}
+      {battleResult && <h3>{battleResult}</h3>}
+    </div>
+  );
 }
 
 export default Fight;
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,70 +1,92 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Pokemon = () => {
-    let navigate = useNavigate()
-    const { id } = useParams()
-    const [pokemon, setPokemon] = useState({
-
-        sprites: {},
-    })
-    const [pokemonInfo, setPokemonInfo] = useState({
-        abilities: []
-
-    })
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    useEffect(() => {
-        fetchPokemon()
-    }, [])
-    const fetchPokemon = async () => {
-
-
-        try {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-            console.log(response.data);
-            setPokemon(response.data)
-            setPokemonInfo(response.data)
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        }
+  let navigate = useNavigate();
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState({
+    sprites: {},
+  });
+  const [pokemonInfo, setPokemonInfo] = useState({
+    abilities: [],
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+  const fetchPokemon = async () => {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      console.log(response.data);
+      setPokemon(response.data);
+      setPokemonInfo(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-    if (loading) {
-        return <h1>Loading...</h1>
-    }
-    if (error) {
-        return <h1>Error</h1>
-    }
-    console.log(pokemonInfo);
+  };
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Error</h1>;
+  }
+  console.log(pokemonInfo);
 
-    return (
-        <>
-            <div style={{ textAlign: 'center', border: ' 15px solid #F44E3F', marginBottom: '50px', width: '100%' }}>
+  return (
+    <>
+      <div
+        style={{
+          textAlign: "center",
+          border: " 15px solid #F44E3F",
+          marginBottom: "50px",
+          width: "100%",
+        }}
+      >
+        <p>I am {pokemon.name}</p>
+        <img
+          style={{
+            width: "500px",
+            height: "500px",
+            border: "5px solid green",
+          }}
+          src={pokemon.sprites.front_default}
+        />
 
-
-                <p>I am {pokemon.name}</p>
-                <img style={{ width: '500px', height: '500px', border: '5px solid green' }} src={pokemon.sprites.front_default} />
-
-                <p>Abilities: {pokemonInfo.abilities[0].ability.name}</p>
-                <p>Abilities:{pokemonInfo.abilities[1].ability.name}</p>
-                <p>Moves: {pokemonInfo.moves[0].move.name}</p>
-                <p>Moves: {pokemonInfo.moves[20].move.name}</p>
-                <p>Hp: {pokemonInfo.stats[0].base_stat}</p>
-                <p>Stats: {pokemonInfo.stats[3].base_stat} - {pokemonInfo.stats[3].stat.name}</p>
-                <p>Stats: {pokemonInfo.stats[4].base_stat} - {pokemonInfo.stats[4].stat.name}</p>
-
-            </div>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-                <button>Select a new Pokemon</button>
-                <button >Fight</button>
-            </div>
-        </>
-    )
-}
-export default Pokemon
+        <p>Abilities: {pokemonInfo.abilities[0].ability.name}</p>
+        <p>Abilities:{pokemonInfo.abilities[1].ability.name}</p>
+        <p>Moves: {pokemonInfo.moves[0].move.name}</p>
+        <p>Moves: {pokemonInfo.moves[20].move.name}</p>
+        <p>Hp: {pokemonInfo.stats[0].base_stat}</p>
+        <p>
+          Stats: {pokemonInfo.stats[3].base_stat} -{" "}
+          {pokemonInfo.stats[3].stat.name}
+        </p>
+        <p>
+          Stats: {pokemonInfo.stats[4].base_stat} -{" "}
+          {pokemonInfo.stats[4].stat.name}
+        </p>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <button>Select a new Pokemon</button>
+        <Link to="/fight" state={{ pokemonID: pokemonInfo.id }}>
+          Fight
+        </Link>
+      </div>
+    </>
+  );
+};
+export default Pokemon;
