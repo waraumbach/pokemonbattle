8512ee773d435b40e51fb2ff0a03a6b57d7cfb26
peferetin <peferetin@yahoo.com>
2024-05-16 01:11:27 +0200

    added

--- src/components/Fight.jsx
+++ src/components/Fight.jsx
@@ -1,124 +1,9 @@
 
 
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import { useLocation } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-
-// function Fight() {
-//   let { state } = useLocation();
-//   const [selectedPokemon, setSelectedPokemon] = useState(null);
-//   const [randomPokemon, setRandomPokemon] = useState(null);
-//   const [selectedPokemonHP, setSelectedPokemonHP] = useState(0);
-//   const [randomPokemonHP, setRandomPokemonHP] = useState(0);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const [battleStarted, setBattleStarted] = useState(false);
-
-//   let navigate = useNavigate();
-
-//   const fetchPokemon = async () => {
-//     try {
-//       if (state) {
-//         const response = await axios.get(
-//           `https://pokeapi.co/api/v2/pokemon/${state.pokemonID}`
-//         );
-//         setSelectedPokemon(response.data);
-//         setSelectedPokemonHP(response.data.stats[0].base_stat);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching Pokemon:", error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   const fetchRandomPokemon = async () => {
-//     try {
-//       const response = await axios.get(
-//         `https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random() * 150) + 1}`
-//       );
-//       setRandomPokemon(response.data);
-//       setRandomPokemonHP(response.data.stats[0].base_stat);
-//     } catch (error) {
-//       console.error("Error fetching Pokemon:", error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   const startBattle = () => {
-//     setBattleStarted(true);
-//   }
-
-//   const attack = (attacker, defender, setDefenderHP) => {
-//     const damage = Math.floor(Math.random() * 20) + 1; // Random damage between 1 and 20
-//     alert(`${attacker} does ${damage} damage!`);
-//     setDefenderHP(defender - damage);
-//     if (defender - damage <= 0) {
-//       alert(`${attacker} wins!`);
-//       setBattleStarted(false);
-//     }
-//   }
-
-//   const refillStrength = (pokemon, setPokemonHP) => {
-//     setPokemonHP(prevHP => prevHP + 20);
-//   }
-
-//   useEffect(() => {
-//     fetchPokemon();
-//     fetchRandomPokemon();
-//   }, []);
-
-//   if (loading) return <p>Loading...</p>
-//   if (error) return <p>Error</p>
-
-//   return (
-//     <>
-//       <div>
-//         <h1>Pokemon Game</h1>
-
-//         {!battleStarted && <button onClick={startBattle}>Start Battle</button>}
-//         {battleStarted && (
-//           <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-//             <button onClick={() => attack(selectedPokemon.name, randomPokemonHP, setRandomPokemonHP)}>Attack</button>
-//             <button onClick={() => refillStrength(selectedPokemon.name, setSelectedPokemonHP)}>Refill Strength</button>
-//             <button onClick={() => attack(randomPokemon.name, selectedPokemonHP, setSelectedPokemonHP)}>Counter Attack</button>
-
-//           </div>
-//         )}
-//         <div style={{ display: 'flex' }}>
-//           {selectedPokemon && (
-//             <div>
-//               <h2>Your Pokemon: {selectedPokemon.name} (HP: {selectedPokemonHP})</h2>
-//               <img className="pokemon-image"
-//                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`}
-//                 alt={selectedPokemon.name}
-//               />
-//             </div>
-//           )}
-//           <h2>VS</h2>
-//           {randomPokemon && (
-//             <div>
-//               <h2>Opponent's Pokemon: {randomPokemon.name} (HP: {randomPokemonHP})</h2>
-//               <img className="pokemon-image"
-//                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPokemon.id}.png`}
-//                 alt={randomPokemon.name}
-//               />
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//       <button onClick={() => navigate('/pokemons')}>Use another Pokemon</button>
-//     </>
-//   );
-// }
-
-// export default Fight;
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Fight() {
   let { state } = useLocation();
@@ -129,6 +14,8 @@ function Fight() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [battleStarted, setBattleStarted] = useState(false);
+  const [battleMessage, setBattleMessage] = useState('');
+  let navigate = useNavigate();
 
   const fetchPokemon = async () => {
     try {
@@ -162,37 +49,42 @@ function Fight() {
 
   const startBattle = () => {
     setBattleStarted(true);
+
   }
 
-  const attack = (attacker, defender, setDefenderHP) => {
+
+  const attack = (attacker, defender, setDefenderHP, counterAttack = false) => {
     const damage = Math.floor(Math.random() * 20) + 1; // Random damage between 1 and 20
-    alert(`${attacker} does ${damage} damage!`);
+    setBattleMessage(`${attacker} does ${damage} damage!`);
     setDefenderHP(defender - damage);
     if (defender - damage <= 0) {
-      alert(`${attacker} wins!`);
+      setBattleMessage(`${attacker} wins!`);
       setBattleStarted(false);
+    } else if (!counterAttack) {
+      // If this is not a counter-attack, schedule a counter-attack in 3 seconds
+      setTimeout(() => attack(randomPokemon.name, selectedPokemonHP, setSelectedPokemonHP, true), 3000);
     }
   }
 
-  const refillStrength = (pokemon, setPokemonHP) => {
-    if (selectedPokemonHP < randomPokemonHP) {
-      setPokemonHP(prevHP => prevHP + 20);
-    }
+
+  // const refillStrength = (pokemon, setPokemonHP) => {
+  //   setPokemonHP(prevHP => prevHP + 20);
+  // }
+
+  const refillStrength = (pokemon, setPokemonHP, opponentHP, setOpponentHP) => {
+    setPokemonHP(prevHP => {
+      if (prevHP > opponentHP) {
+        setOpponentHP(prevOpponentHP => prevOpponentHP + 20);
+        return prevHP + 20;
+      }
+      return prevHP;
+    });
   }
 
   useEffect(() => {
     fetchPokemon();
     fetchRandomPokemon();
   }, []);
-
-  useEffect(() => {
-    if (battleStarted) {
-      const timer = setTimeout(() => {
-        attack(randomPokemon.name, selectedPokemonHP, setSelectedPokemonHP);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [selectedPokemonHP]);
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error</p>
@@ -204,13 +96,22 @@ function Fight() {
 
         {!battleStarted && <button onClick={startBattle}>Start Battle</button>}
         {battleStarted && (
-          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+          <div className="buttons">
             <button onClick={() => attack(selectedPokemon.name, randomPokemonHP, setRandomPokemonHP)}>Attack</button>
-            <button onClick={() => refillStrength(selectedPokemon.name, setSelectedPokemonHP)}>Refill Strength</button>
+            {/* <button onClick={() => refillStrength(selectedPokemon.name, setSelectedPokemonHP)}>Refill Strength</button> */}
+            <button onClick={() => refillStrength(selectedPokemon.name, setSelectedPokemonHP, randomPokemonHP, setRandomPokemonHP)}>Refill Strength</button>
+            <button onClick={() => attack(randomPokemon.name, selectedPokemonHP, setSelectedPokemonHP)}>Counter Attack</button>
           </div>
         )}
+
+
+
+
+
         <div style={{ display: 'flex' }}>
-          {selectedPokemon && (
+
+
+          {/* {selectedPokemon && (
             <div>
               <h2>Your Pokemon: {selectedPokemon.name} (HP: {selectedPokemonHP})</h2>
               <img className="pokemon-image"
@@ -228,9 +129,37 @@ function Fight() {
                 alt={randomPokemon.name}
               />
             </div>
+          )} */}
+
+          {selectedPokemon && (
+            <div>
+              <h2>Your Pokemon: {selectedPokemon.name}</h2>
+              <div className="hp-bar">
+                <div className={`hp-bar-inner ${selectedPokemonHP <= 50 ? 'low' : ''} ${selectedPokemonHP <= 20 ? 'critical' : ''}`} style={{ width: `${selectedPokemonHP}%` }}></div>
+              </div>
+              <img className="pokemon-image"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png`}
+                alt={selectedPokemon.name}
+              />
+            </div>
+          )}
+          <h2>VS</h2>
+          {randomPokemon && (
+            <div>
+              <h2>Opponent's Pokemon: {randomPokemon.name}</h2>
+              <div className="hp-bar">
+                <div className={`hp-bar-inner ${randomPokemonHP <= 50 ? 'low' : ''} ${randomPokemonHP <= 20 ? 'critical' : ''}`} style={{ width: `${randomPokemonHP}%` }}></div>
+              </div>
+              <img className="pokemon-image"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomPokemon.id}.png`}
+                alt={randomPokemon.name}
+              />
+            </div>
           )}
         </div>
+        <p style={{ color: 'black', fontWeight: 'bold', fontSize: '20px' }}>{battleMessage}</p>
       </div>
+      <button style={{ marginTop: '20px', padding: '10px' }} onClick={() => navigate('/pokemons')}>Select another Pokemon</button>
     </>
   );
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -42,43 +42,113 @@ const Pokemon = () => {
   console.log(pokemonInfo);
 
   return (
+    // <>
+    //   <div className="pokemon-container">
+    //     <h1>I am {pokemon.name}</h1>
+    //     <img className="pokemon-image" src={pokemon.sprites.front_default} />
+    //     <div className="stats-pokemon">
+    //       <h3>HP: {pokemonInfo.stats[0].base_stat}</h3>
+    //       <h3>Abilities: </h3> <p> {pokemonInfo.abilities[0].ability.name}</p> +
+    //       <p> {pokemonInfo.abilities[1].ability.name}</p>
+    //       <h3>Moves: </h3> <p> {pokemonInfo.moves[0].move.name}</p> +
+    //       <p> {pokemonInfo.moves[20].move.name}</p>
+    //       <h3>Stats: </h3>{" "}
+    //       <p>
+    //         {pokemonInfo.stats[3].base_stat} - {pokemonInfo.stats[3].stat.name}
+    //       </p>{" "}
+    //       +
+    //       <p>
+    //         {pokemonInfo.stats[2].base_stat} - {pokemonInfo.stats[2].stat.name}
+    //       </p>{" "}
+    //       +
+    //       <p>
+    //         {pokemonInfo.stats[4].base_stat} - {pokemonInfo.stats[4].stat.name}{" "}
+    //       </p>
+    //     </div>
+    //   </div>
+    //   <div
+    //     style={{
+    //       width: "100%",
+    //       display: "flex",
+    //       justifyContent: "space-between",
+    //     }}
+    //   >
+    //     <button onClick={() => navigate('/pokemons')}>Select a new Pokemon</button>
+    //     <Link to="/fight" state={{ pokemonID: pokemonInfo.id }}>
+    //       Fight
+    //     </Link>
+    //   </div>
+    // </>
+
+
+
+    // <>
+    //   <div className="pokemon-container">
+    //     <h1>I am {pokemon.name}</h1>
+    //     <img className="pokemon-image" src={pokemon.sprites.front_default} />
+    //     <div className="stats-pokemon">
+    //       <div className="stat">
+    //         <span>HP:</span>
+    //         <div className="bar hp" style={{ '--hp': pokemonInfo.stats[0].base_stat }}></div>
+    //         <span>{pokemonInfo.stats[0].base_stat}</span>
+    //       </div>
+    //       <div className="stat">
+    //         <span>Abilities:</span>
+    //         <div className="bar hp" style={{ '--hp': pokemonInfo.abilities[0].ability.name }}></div>
+    //         <span>{pokemonInfo.abilities[0].ability.name}</span>
+    //       </div>
+
+    //     </div>
+    //   </div>
+    //   <div className="button-container">
+    //     <button onClick={() => navigate('/pokemons')}>Select a new Pokemon</button>
+    //     <Link to="/fight" state={{ pokemonID: pokemonInfo.id }}>
+    //       Fight
+    //     </Link>
+    //   </div>
+    // </>
     <>
       <div className="pokemon-container">
         <h1>I am {pokemon.name}</h1>
         <img className="pokemon-image" src={pokemon.sprites.front_default} />
         <div className="stats-pokemon">
-          <h3>HP: {pokemonInfo.stats[0].base_stat}</h3>
-          <h3>Abilities: </h3> <p> {pokemonInfo.abilities[0].ability.name}</p> +
-          <p> {pokemonInfo.abilities[1].ability.name}</p>
-          <h3>Moves: </h3> <p> {pokemonInfo.moves[0].move.name}</p> +
-          <p> {pokemonInfo.moves[20].move.name}</p>
-          <h3>Stats: </h3>{" "}
-          <p>
-            {pokemonInfo.stats[3].base_stat} - {pokemonInfo.stats[3].stat.name}
-          </p>{" "}
-          +
-          <p>
-            {pokemonInfo.stats[2].base_stat} - {pokemonInfo.stats[2].stat.name}
-          </p>{" "}
-          +
-          <p>
-            {pokemonInfo.stats[4].base_stat} - {pokemonInfo.stats[4].stat.name}{" "}
-          </p>
+          <div className="stat">
+            <span>HP:</span>
+            <div className="bar hp" style={{ '--hp': pokemonInfo.stats[0].base_stat }}></div>
+            <span>{pokemonInfo.stats[0].base_stat}</span>
+          </div>
+          <div className="stat">
+            <span>Abilities:</span>
+            <div className="bar ability" style={{ '--ability': pokemonInfo.abilities.length }}></div>
+            <span>{pokemonInfo.abilities[0].ability.name}</span>
+
+            {/* , {pokemonInfo.abilities[1]?.ability.name} */}
+          </div>
+          <div className="stat">
+            <span>Moves:</span>
+            <div className="bar moves" style={{ '--moves': pokemonInfo.moves.length }}></div>
+            <span>{pokemonInfo.moves[0].move.name}</span>
+
+            {/* ,{pokemonInfo.moves[20]?.move.name} */}
+          </div>
+          {pokemonInfo.stats.slice(1).map((stat, index) => (
+            <div key={index} className="stat">
+              <span>{stat.stat.name}:</span>
+              <div className={`bar stat-${index}`} style={{ [`--stat-${index}`]: stat.base_stat }}></div>
+              <span>{stat.base_stat}</span>
+            </div>
+          ))}
         </div>
       </div>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
+      <div className="button-container">
         <button onClick={() => navigate('/pokemons')}>Select a new Pokemon</button>
         <Link to="/fight" state={{ pokemonID: pokemonInfo.id }}>
           Fight
         </Link>
       </div>
     </>
+
+
   );
 };
 export default Pokemon;
